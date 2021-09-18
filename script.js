a51bf9a6c44fb9659e8a6c983f9f97c9b13fd00a
Mehul Mohan <mehulmpt@gmail.com>
2021-09-19 02:53:02 +0530

    lab

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-async function getScript(url) {
-	// download and execute the script
+function generateSelector(target) {
+	return 'TODO: Implementation missing'
 }
+
+document.body.addEventListener('click', (e) => {
+	let selector = document.querySelector('.selector')
+	// selector output
+	let output = generateSelector(e.target)
+
+	selector.innerHTML = `<strong>Selector:</strong> ${output}`
+
+	// element that you select
+	let element = document.querySelector(output)
+	console.log('Selected Element:', element)
+})
